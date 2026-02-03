75dedc95b96fdb0908a8aee4a55c2f084b9517e9
Oli <palmbusiness12@gmail.com>
2026-02-03 19:35:35 +0000

    Added persistent storage with useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ToDos from "./components/ToDos";
 import Entry from "./components/Entry";
 
 function App() {
     const [currentValue, setCurrentValue] = useState("");
-    const [toDos, setToDos] = useState([]);
+    const [toDos, setToDos] = useState(() => {
+        const saved = localStorage.getItem("toDos");
+        const initalValue = JSON.parse(saved);
+        return initalValue || [];
+    });
+
+    useEffect(() => {
+        localStorage.setItem("toDos", JSON.stringify(toDos));
+    }, [toDos]);
 
     function handleAddClick() {
         if (currentValue == "") {
@@ -13,8 +21,10 @@ function App() {
         }
 
         let newToDo = { value: currentValue, id: Date.now() };
+        let newList = [...toDos, newToDo];
 
-        setToDos([...toDos, newToDo]);
+        setToDos(newList);
+
         setCurrentValue("");
     }
 
